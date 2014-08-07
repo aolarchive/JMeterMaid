b553edd7365357e947419619e21bc4b136dd573e
Drew Ansbacher <drew.ansbacher@aol.com>
2014-08-07 15:27:53 -0400

    PostDestroy added

--- src/main/java/com/web_application/RunImmediately.java
+++ src/main/java/com/web_application/RunImmediately.java
@@ -2,15 +2,20 @@ package com.web_application;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
 import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+
+import com.ThreadPoolExec;
 
 @Component
 public class RunImmediately {
@@ -26,10 +31,14 @@ public class RunImmediately {
 
 	GitManipulator git = new GitManipulator();
 	
+	ThreadPoolExecutor tpe = ThreadPoolExec.getThreadPoolExec().getExecutor();
 
 	@PostConstruct
 	public void go() throws IOException, SchedulerException,
 			InvalidRemoteException, TransportException, GitAPIException {
+		
+		
+		
 		EnvironmentEntity e = new EnvironmentEntity();
 		e.setName("OBI01");
 		e.setCron("0 50 10 * * ?");
@@ -48,6 +57,15 @@ public class RunImmediately {
 		}
 		git.cloneOrPull();
 		trigger.triggerCron();
+	}
+	
+	@PreDestroy
+	public void kill() throws InterruptedException
+	{
+		while(tpe.getActiveCount() > 0)
+		{
+			Thread.sleep(5000);
+		}
 	}
 
 }
